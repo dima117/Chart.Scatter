e94de207e09894caf238a60ef185c56b046fec6d
dima117 <dima117a@gmail.com>
2015-04-07 23:29:56 +0300

    format dates;

--- Chart.DateFnLine.js
+++ Chart.DateFnLine.js
@@ -1,27 +1,125 @@
 ﻿(function () {
 	"use strict";
 
+	var dateFormat = function () {
+
+		var token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
+			timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
+			timezoneClip = /[^-+\dA-Z]/g,
+			pad = function (val, len) {
+				val = String(val);
+				len = len || 2;
+				while (val.length < len) val = "0" + val;
+				return val;
+			},
+			masks = {
+				"default": "ddd mmm dd yyyy HH:MM:ss"
+			},
+			i18n = {
+				dayNames: [
+					"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
+					"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+				],
+				monthNames: [
+					"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+					"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+				]
+			};
+
+
+
+		// Regexes and supporting functions are cached through closure
+		return function (date, mask, utc) {
+
+			// You can't provide utc if you skip other args (use the "UTC:" mask prefix)
+			if (arguments.length == 1 && Object.prototype.toString.call(date) == "[object String]" && !/\d/.test(date)) {
+				mask = date;
+				date = undefined;
+			}
+
+			// Passing date through Date applies Date.parse, if necessary
+			date = date ? new Date(date) : new Date;
+			if (isNaN(date)) throw SyntaxError("invalid date");
+
+			mask = String(masks[mask] || mask || masks["default"]);
+
+			// Allow setting the utc argument via the mask
+			if (mask.slice(0, 4) == "UTC:") {
+				mask = mask.slice(4);
+				utc = true;
+			}
+
+			var _ = utc ? "getUTC" : "get",
+				d = date[_ + "Date"](),
+				D = date[_ + "Day"](),
+				m = date[_ + "Month"](),
+				y = date[_ + "FullYear"](),
+				H = date[_ + "Hours"](),
+				M = date[_ + "Minutes"](),
+				s = date[_ + "Seconds"](),
+				L = date[_ + "Milliseconds"](),
+				o = utc ? 0 : date.getTimezoneOffset(),
+				flags = {
+					d: d,
+					dd: pad(d),
+					ddd: i18n.dayNames[D],
+					dddd: i18n.dayNames[D + 7],
+					m: m + 1,
+					mm: pad(m + 1),
+					mmm: i18n.monthNames[m],
+					mmmm: i18n.monthNames[m + 12],
+					yy: String(y).slice(2),
+					yyyy: y,
+					h: H % 12 || 12,
+					hh: pad(H % 12 || 12),
+					H: H,
+					HH: pad(H),
+					M: M,
+					MM: pad(M),
+					s: s,
+					ss: pad(s),
+					l: pad(L, 3),
+					L: pad(L > 99 ? Math.round(L / 10) : L),
+					t: H < 12 ? "a" : "p",
+					tt: H < 12 ? "am" : "pm",
+					T: H < 12 ? "A" : "P",
+					TT: H < 12 ? "AM" : "PM",
+					Z: utc ? "UTC" : (String(date).match(timezone) || [""]).pop().replace(timezoneClip, ""),
+					o: (o > 0 ? "-" : "+") + pad(Math.floor(Math.abs(o) / 60) * 100 + Math.abs(o) % 60, 4),
+					S: ["th", "st", "nd", "rd"][d % 10 > 3 ? 0 : (d % 100 - d % 10 != 10) * d % 10]
+				};
+
+			return mask.replace(token, function ($0) {
+				return $0 in flags ? flags[$0] : $0.slice(1, $0.length - 1);
+			});
+		};
+	}();
+
 	var chartjs = this,
 		helpers = chartjs.helpers,
 		hlp = {
-			
-			formatDate: function(date) {
+
+			formatDateValue: function (date) {
 
 				date = new Date(+date);
 
-				var norm = new Date(+date + date.getTimezoneOffset() * 3600000);
+				var ms = date.getUTCMilliseconds();
 
-				var hasTime = norm.getHours() +
-					norm.getMinutes() +
-					norm.getSeconds() +
-					norm.getMilliseconds();
+				if (ms) {
+
+					return ('000' + ms).slice(-3);
+				}
+
+				var hasTime = date.getUTCHours() +
+					date.getUTCMinutes() +
+					date.getUTCSeconds();
 
 				if (hasTime) {
 
-					return norm.toLocaleTimeString();
+					return dateFormat(date, "h:MM", true);
 				} else {
-					
-					return norm.toLocaleDateString();
+
+					return dateFormat(date, "mmm d", true);
 				}
 			}
 		};
@@ -114,7 +212,7 @@
 				var value = graphMin + stepValue * index;
 
 
-				labelsArray[index] = hlp.formatDate(value);
+				labelsArray[index] = hlp.formatDateValue(value);
 			});
 
 			this.xLabels = labelsArray;
@@ -122,17 +220,16 @@
 	});
 
 	chartjs.types.FnLine.extend({
-		// Passing in a name registers this chart in the Chart namespace in the same way
+
 		name: "DateFnLine",
-		//,		initialize: function (data) {
-		//	console.log('My Line chart extension');
-		//	Chart.types.Line.prototype.initialize.apply(this, arguments);
-		//}
+		defaults: {
 
+			scaleArgLabel: "mmm d, yyyy, hh:MM",
+		},
 		scaleClass: chartjs.DateFnScale,
-		formatLabelX: function(arg) {
+		formatLabelX: function (arg) {
 
-			return hlp.formatDate(arg);
+			return dateFormat(arg, this.options.scaleArgLabel, true);
 		}
 	});
 
